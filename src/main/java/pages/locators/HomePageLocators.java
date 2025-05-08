3b863563f6334f3766b4e193357b01f8487c5ca7
LidorTsunami <118756066+LidorTsunami@users.noreply.github.com>
2025-05-09 01:13:33 +0300

    refactoring

--- src/main/java/pages/locators/HomePageLocators.java
+++ src/main/java/pages/locators/HomePageLocators.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.By;
 public class HomePageLocators {
     public static final By CART_ICON = By.id("shopping_cart_container");
 
-    // Dynamic locator pattern for product add-to-cart buttons
     public static By addToCartButtonByProductName(String productName) {
         return By.xpath("//*[text()='" + productName + "']/ancestor::div[@class='inventory_item']//button[text()='Add to cart']");
     }
